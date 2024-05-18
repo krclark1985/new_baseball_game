40a13f4e97b2d9ad0ab490b5bce1786b37e5260a
kylec <krclark8485@hotmail.com>
2024-05-18 16:44:24 -0700

    fix #3 for eb application module issue; focused on __init__ changes

--- src/__init__.py
+++ src/__init__.py
@@ -15,9 +15,9 @@ def get_env_variable(name):
 
 def create_app(test_config=None):
     env = get_env_variable("DATABASE_URI_AWS")
-    app = Flask(__name__, instance_relative_config=True)
-    CORS(app)
-    app.config.from_mapping(
+    application = app = Flask(__name__, instance_relative_config=True)
+    CORS(application)
+    application.config.from_mapping(
         SECRET_KEY='dev',
         SQLALCHEMY_DATABASE_URI= env,
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
@@ -26,27 +26,27 @@ def create_app(test_config=None):
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        application.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
-        app.config.from_mapping(test_config)
+        application.config.from_mapping(test_config)
 
     # ensure the instance folder exists
     try:
-        os.makedirs(app.instance_path)
+        os.makedirs(application.instance_path)
     except OSError:
         pass
 
     from .baseball_models import db
-    db.init_app(app)
-    migrate = Migrate(app, db)
+    db.init_app(application)
+    migrate = Migrate(application, db)
 
     from .api import players, teams, game, lineups
-    app.register_blueprint(players.bp)
-    app.register_blueprint(teams.bp)
-    app.register_blueprint(game.bp)
-    app.register_blueprint(lineups.bp)
+    application.register_blueprint(players.bp)
+    application.register_blueprint(teams.bp)
+    application.register_blueprint(game.bp)
+    application.register_blueprint(lineups.bp)
     # app.register_blueprint(team1lineup.bp)
     # app.register_blueprint(team2lineup.bp)
 
-    return app
+    return application
